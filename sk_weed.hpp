2504b002277eaa66bfe3742a272074094b4727af
Simon Harris <simonrharris@users.noreply.github.com>
2018-04-13 20:45:29 +0100

    Delete sk_weed.hpp

--- sk_weed.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __KMERS_HPP_INCLUDED__
-#define __KMERS_HPP__
-#endif
-
-#include <string> //std::string
-
-using namespace std;
-
-int weedKmers(string kmerfile, string weedfile, string outputfile);
