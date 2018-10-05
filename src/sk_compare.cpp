2f1b8f7e20142f5f9c2e5f288c27a9e30f51d7d1
Simon Harris <sh16@sanger.ac.uk>
2018-10-05 09:54:17 +0100

    Made big efficiency improvements in sk compare

--- src/sk_compare.cpp
+++ src/sk_compare.cpp
@@ -13,161 +13,137 @@ int compareKmerFiles(const std::string & queryfile, const std::vector < std::str
 {
 
 	// Create the kmer map
-	std::unordered_map < std::string, std::string > kmerMap;
+	std::unordered_map < std::string, char > kmerMap;
 
-	std::vector < std::string > sampleNames;
-	if (collectSampleNames(subjectfiles, sampleNames, false)!=0){return 1;}
-	int numSamples=sampleNames.size();
-	std::string emptySequence (numSamples , '-');
-	std::vector < int > queryKmers (numSamples, 0);
+	//std::vector < std::string > sampleNames;
+	//if (collectSampleNames(subjectfiles, sampleNames, false)!=0){return 1;}
+	//int numSamples=sampleNames.size();
+	//std::string emptySequence (numSamples , '-');
+	
 
-	int sampleNum=0;
-	int subjectkmersize;
-	int oldsubjectkmersize;
-	std::vector < std::string > subjectSampleNames;
+	int querykmersize;
+	std::vector < std::string > querySampleNames;
 
 	std::ifstream fileStream;
 
-	for (int s = 0; s < subjectfiles.size(); ++s){
+	if (openFileStream(queryfile, fileStream, false)){return 1;};		
 		
-		if (openFileStream(subjectfiles[s], fileStream, false)){return 1;};		
-		
-		readKmerHeader(fileStream, subjectkmersize, subjectSampleNames);
+	readKmerHeader(fileStream, querykmersize, querySampleNames);
 
-		if (s==0){
-			oldsubjectkmersize=subjectkmersize;
+	char base;
+	char kmerbuffer[querykmersize*2/3];
+	char asciibuffer[int(ceil(float(querySampleNames.size())/6))];
+	while (fileStream.read(asciibuffer, sizeof(asciibuffer))){//read the ascii representation of the taxa
+		std::string asciibits (asciibuffer, sizeof(asciibuffer));
+		//std::vector < bool > mybits;
+		//vectorbool_from_ascii(asciibits, mybits);//read the ascii representation of the taxa to a verctor of bools
+		while (fileStream.peek()!='\n' && fileStream.get(base)){
+			fileStream.read(kmerbuffer, sizeof(kmerbuffer));
+			std::string kmer (kmerbuffer, querykmersize*2/3);
+			
+			std::unordered_map < std::string, char >::iterator it = kmerMap.find(kmer);//check if the kmer is in the hash
+			if ( it != kmerMap.end() ){//if the kmer is in the hash
+				it->second='N';
+			}
+			else {
+				kmerMap.insert(std::make_pair(kmer, base));
+			}
 		}
+		fileStream.ignore(256,'\n');//skip the end ofline character
+	}
+	querySampleNames.clear();
+	fileStream.close();
 
-		if (subjectkmersize!=oldsubjectkmersize){
-			std::cerr << "kmer files have different kmer sizes" << std::endl <<std::endl;
+	std::cout << "Subject\tKmers unique to Query\tKmers unique to Subject\tMatches\t% kmers in Query matching\t% kmers in Subject matching\tSNPs\t%ID of matching kmers\t%ID of Query kmers\t%ID of Subject kmers\tNs in Query\tNs in Subject\tNs in both" << std::endl;
+	
+	for (int s = 0; s < subjectfiles.size(); ++s){
+		fileStream.open(subjectfiles[s], std::ios::in);
+		if (fileStream.fail()) {
+			std::cerr << "Failed to open " << subjectfiles[s] << std::endl << std::endl;
 			return 1;
 		}
+		int subjectkmersize;
+		std::vector < std::string > subjectSampleNames;
+		
+		try {
+				int returnval = readKmerHeader(fileStream, subjectkmersize, subjectSampleNames);
+			}
+			catch (int e){
+				std::cerr << "An exception occurred when reading file " << subjectfiles[s] << ". Please check the format. Exception Nr. " << e << std::endl;
+				return 1;
+			}
+		if (subjectkmersize!=querykmersize){
+			std::cout << subjectfiles[s] << " has different kmer size to query" << std::endl;
+			return 1;
+		}
+
+		
+		std::vector < int > kmerjustinsubject (subjectSampleNames.size(), 0);
+		std::vector < int > snps (subjectSampleNames.size(), 0);
+		std::vector < int > ninsubject (subjectSampleNames.size(), 0);
+		std::vector < int > ninquery (subjectSampleNames.size(), 0);
+		std::vector < int > ninboth (subjectSampleNames.size(), 0);
+		std::vector < int > matches (subjectSampleNames.size(), 0);
+
 		char base;
-		char kmerbuffer[subjectkmersize*2/3];
+		char kmerbuffer[querykmersize*2/3];
 		char asciibuffer[int(ceil(float(subjectSampleNames.size())/6))];
+
 		while (fileStream.read(asciibuffer, sizeof(asciibuffer))){//read the ascii representation of the taxa
 			std::string asciibits (asciibuffer, sizeof(asciibuffer));
 			std::vector < bool > mybits;
 			vectorbool_from_ascii(asciibits, mybits);//read the ascii representation of the taxa to a verctor of bools
 			while (fileStream.peek()!='\n' && fileStream.get(base)){
+				base=toupper(base);
 				fileStream.read(kmerbuffer, sizeof(kmerbuffer));
-				std::string kmer (kmerbuffer, subjectkmersize*2/3);
-				
-				std::unordered_map < std::string, std::string >::iterator it = kmerMap.find(kmer);//check if the kmer is in the hash
+				std::string kmer (kmerbuffer, querykmersize*2/3);
+
+				std::unordered_map < std::string, char >::iterator it = kmerMap.find(kmer);//check if the kmer is in the hash
 				if ( it != kmerMap.end() ){//if the kmer is in the hash
 					for (int i=0; i<subjectSampleNames.size(); ++i){ //add the base to all samples that are true in the bitset
 						if (mybits[i]){
-							it->second[i+sampleNum]=base;
-							queryKmers[i+sampleNum]++;
+							if (it->second=='N' && base=='N'){
+								ninboth[i]++;
+							}
+							else if (it->second=='N'){
+								ninquery[i]++;
+							}
+							else if (base=='N'){
+								ninsubject[i]++;
+							}
+							else if (it->second==base){
+								matches[i]++;
+							}
+							else {
+								snps[i]++;
+							}
 						}
 					}
 				}
 				else {
-					std::string newsequence = emptySequence;
 					for (int i=0; i<subjectSampleNames.size(); ++i){ //add the base to all samples that are true in the bitset
 						if (mybits[i]){
-							newsequence[i+sampleNum]=base;
-							queryKmers[i+sampleNum]++;
+							kmerjustinsubject[i]++;
 						}
 					}
-					kmerMap.insert(std::make_pair(kmer, newsequence));
 				}
-			}
-			fileStream.ignore(256,'\n');//skip the end ofline character
-		}
-		sampleNum+=int(subjectSampleNames.size()); //add the number of samples in the file to the count of total samples
-		subjectSampleNames.clear();
+	    	}
+	    	fileStream.ignore(256,'\n');//skip the end ofline character
+	    }
 		fileStream.close();
-	}
-	
-	fileStream.open(queryfile, std::ios::in);
-	if (fileStream.fail()) {
-		std::cerr << "Failed to open " << queryfile << std::endl << std::endl;
-		return 1;
-	}
-	int querykmersize;
-	std::vector < std::string > querySampleNames;
-	if (querySampleNames.size()>1){
-		std::cerr << "Error: Query can only be a single sample, not a kmerge" << std::endl;
-		return 1;
-	}
-	
-	try {
-			int returnval = readKmerHeader(fileStream, querykmersize, querySampleNames);
-		}
-		catch (int e){
-			std::cerr << "An exception occurred when reading file " << queryfile << ". Please check the format. Exception Nr. " << e << std::endl;
-			return 1;
-		}
-	if (subjectkmersize!=querykmersize){
-		std::cout << queryfile << " has different kmer size to query" << std::endl;
-		return 1;
-	}
-
-	std::cout << "Subject\tKmers unique to Query\tKmers unique to Subject\tMatches\t% kmers in Query matching\t% kmers in Subject matching\tSNPs\t%ID of matching kmers\t%ID of Query kmers\t%ID of Subject kmers\tNs in Query\tNs in Subject\tNs in both" << std::endl;
-	
-	std::vector < int > kmerjustinb (sampleNames.size(), 0);
-	std::vector < int > snps (sampleNames.size(), 0);
-	std::vector < int > nina (sampleNames.size(), 0);
-	std::vector < int > ninb (sampleNames.size(), 0);
-	std::vector < int > ninboth (sampleNames.size(), 0);
-	std::vector < int > matches (sampleNames.size(), 0);
-
-	char base;
-	char kmerbuffer[querykmersize*2/3];
-	char asciibuffer[int(ceil(float(querySampleNames.size())/6))];
-
-	while (fileStream.read(asciibuffer, sizeof(asciibuffer))){//read the ascii representation of the taxa
-		std::string asciibits (asciibuffer, sizeof(asciibuffer));
-		while (fileStream.peek()!='\n' && fileStream.get(base)){
-			base=toupper(base);
-			fileStream.read(kmerbuffer, sizeof(kmerbuffer));
-			std::string kmer (kmerbuffer, querykmersize*2/3);
-
-			std::unordered_map < std::string, std::string >::iterator it = kmerMap.find(kmer);//check if the kmer is in the hash
-			if ( it != kmerMap.end() ){//if the kmer is in the hash
-				for (int i=0; i<sampleNames.size(); ++i){ //add the base to all samples that are true in the bitset
-					if (it->second[i]=='N' && base=='N'){
-						ninboth[i]++;
-					}
-					else if (it->second[i]=='-'){
-						kmerjustinb[i]++;
-					}
-					else if (it->second[i]=='N'){
-						nina[i]++;
-					}
-					else if (base=='N'){
-						ninb[i]++;
-					}
-					else if (it->second[i]==base){
-						matches[i]++;
-					}
-					else {
-						snps[i]++;
-					}
-				}
-			}
-			else {
-				for (int i=0; i<sampleNames.size(); ++i){ //add the base to all samples that are true in the bitset
-					kmerjustinb[i]++;
-				}
-			}
-    	}
-    	fileStream.ignore(256,'\n');//skip the end ofline character
-    }
-	fileStream.close();
-	
-	for (int i=0; i<sampleNames.size(); ++i){
-
-		int kmerjustina=queryKmers[i]-(matches[i]+snps[i]+nina[i]+ninboth[i]+ninb[i]);
-		float percentmatcha=float(matches[i]+snps[i]+nina[i]+ninboth[i])/(kmerjustina+matches[i]+snps[i]+nina[i]+ninboth[i])*100;
-		float percentmatchb=float(matches[i]+snps[i]+ninb[i]+ninboth[i])/(kmerjustinb[i]+matches[i]+snps[i]+ninb[i]+ninboth[i])*100;
-		float percentidofmatches=float(matches[i])/(matches[i]+snps[i])*100;
-		float percentidofquery=(percentidofmatches*percentmatcha)/100;
-		float percentidofsubject=(percentidofmatches*percentmatchb)/100;
 		
-		std::cout << sampleNames[i] << "\t" << kmerjustina << "\t" << kmerjustinb[i] << "\t" << matches[i] << "\t" << percentmatcha << "\t" << percentmatchb << "\t" << snps[i] << "\t" << percentidofmatches << "\t" << percentidofquery << "\t" << percentidofsubject << "\t" << nina[i] << "\t" << ninb[i] << "\t" << ninboth[i] << std::endl;
+		for (int i=0; i<subjectSampleNames.size(); ++i){
 
+			int kmerjustinquery=kmerMap.size()-(matches[i]+snps[i]+ninquery[i]+ninboth[i]+ninsubject[i]);
+			float percentmatchquery=float(matches[i]+snps[i]+ninquery[i]+ninboth[i])/(kmerjustinquery+matches[i]+snps[i]+ninquery[i]+ninboth[i])*100;
+			float percentmatchsubject=float(matches[i]+snps[i]+ninsubject[i]+ninboth[i])/(kmerjustinsubject[i]+matches[i]+snps[i]+ninsubject[i]+ninboth[i])*100;
+			float percentidofmatches=float(matches[i])/(matches[i]+snps[i])*100;
+			float percentidofquery=(percentidofmatches*percentmatchquery)/100;
+			float percentidofsubject=(percentidofmatches*percentmatchsubject)/100;
+			
+			std::cout << subjectSampleNames[i] << "\t" << kmerjustinquery << "\t" << kmerjustinsubject[i] << "\t" << (matches[i]+snps[i]+ninquery[i]+ninboth[i]+ninsubject[i]) << "\t" << percentmatchquery << "\t" << percentmatchsubject << "\t" << snps[i] << "\t" << percentidofmatches << "\t" << percentidofquery << "\t" << percentidofsubject << "\t" << ninquery[i] << "\t" << ninsubject[i] << "\t" << ninboth[i] << std::endl;
+
+		}
 	}
 	return 0;
 	
